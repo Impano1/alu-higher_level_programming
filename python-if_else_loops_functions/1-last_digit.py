7e3f1a18bfa13284df97e01e7346da059a760f48
root <f.impanoalustudent.com>
2023-02-08 03:09:45 -0800

    down fm

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -2,9 +2,9 @@
 import random
 number = random.randint(-10000, 10000)
 if number > 0:
-x = number % 10
+    x = number % 10
 if number < 0:
-x : ((number x-1) % 10) x-1))
+    x : ((number x-1) % 10) x-1))
 if last number > 5:
     print(f'{number} and is greater than 5')
 elif number == 0:
