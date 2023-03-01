b2ad57a892c3c6ff6e84c75e5a12f9cd66d834dd
root <f.impanoalustudent.com>
2023-03-01 06:00:26 -0800

    f1 is back

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -4,9 +4,9 @@ if __name__ == "__main__":
     length = len(sys.argv)
     if length == 1:
         print("{} arguments.".format(length - 1))
-        elif length == 2:
-            print("{} argument:".format(length - 1))
-            else:
-                print("{} arguments:".format(length - 1))
-                for a in range(1, length):
-                    print("{}: {}".format(a, sys.argv[a]))
+    elif length == 2:
+        print("{} argument:".format(length - 1))
+    else:
+        print("{} arguments:".format(length - 1))
+    for a in range(1, length):
+        print("{}: {}".format(a, sys.argv[a]))
