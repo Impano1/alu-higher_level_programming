4c23b71498d3eb13d5a97cd92cc319e8b424896c
root <f.impanoalustudent.com>
2023-03-17 07:48:07 -0700

    real madrid vs chelsea

--- python-more_classes/101-nqueens.py
+++ python-more_classes/101-nqueens.py
@@ -1,40 +1,43 @@
 #!/usr/bin/python3
 import sys
 
-# check if n is valid
-if len(sys.argv) != 2:
-    print("Usage: nqueens N")
-    sys.exit(1)
+def n_queens(N):
+    def is_valid(board, row, col):
+        for i in range(row):
+            if board[i] == col or \
+               board[i] - i == col - row or \
+               board[i] + i == col + row:
+                return False
+        return True
 
-try:
-    n = int(sys.argv[1])
-except ValueError:
-    print("N must be a number")
-    sys.exit(1)
+    def solve(board, row):
+        if row == N:
+            solutions.append(board.copy())
+            return
+        for col in range(N):
+            if is_valid(board, row, col):
+                board[row] = col
+                solve(board, row + 1)
+                board[row] = -1
 
-if n < 4:
-    print("N must be at least 4")
-    sys.exit(1)
+    solutions = []
+    board = [-1] * N
+    solve(board, 0)
+    return solutions
 
-# check if the queen in column x conflicts with the queens in the previous columns
-def is_valid(board, x, y):
-    for i in range(x):
-        if board[i] == y or abs(i - x) == abs(board[i] - y):
-            return False
-    return True
-
-# recursively find all solutions
-def n_queens(board, x):
-    if x == n:
-        print(board)
-        return
-    for y in range(n):
-        if is_valid(board, x, y):
-            board.append(y)
-            n_queens(board, x + 1)
-            board.pop()
-
-# solve the problem
-board = []
-n_queens(board, 0)
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+    try:
+        N = int(sys.argv[1])
+    except ValueError:
+        print("N must be a number")
+        sys.exit(1)
+    if N < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+    solutions = n_queens(N)
+    for solution in solutions:
+        print([[i, solution[i]] for i in range(N)])
 
