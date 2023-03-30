7179a52484f33c18923b0296492b09304a8dc768
root <f.impanoalustudent.com>
2023-03-30 14:26:32 -0700

    amavubi nu bufu

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,4 +1,13 @@
-mysql -hlocalhost -uroot -p < privileges.sql
-SHOW GRANTS FOR 'user_0d_1'@'localhost';
-SHOW GRANTS FOR 'user_0d_2'@'localhost';
+-- This script lists the privileges of MySQL users user_0d_1 and user_0d_2 on localhost.
 
+-- Grant all privileges to user_0d_1
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+
+-- Show privileges for user_0d_1
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+
+-- Grant specific privileges to user_0d_2
+GRANT SELECT, INSERT, UPDATE, DELETE ON database.* TO 'user_0d_2'@'localhost';
+
+-- Show privileges for user_0d_2
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
