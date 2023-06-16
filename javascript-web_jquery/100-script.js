48b874d981118dd13be73bc1e7a198ca4cac25fd
root <f.impano@alustudent.com>
2023-06-16 14:36:24 -0700

    montreal circuit

--- javascript-web_jquery/100-script.js
+++ javascript-web_jquery/100-script.js
@@ -1,15 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <title>Holberton School</title>
-    <script type="text/javascript" src="100-script.js"></script>
-  </head>
-  <body>
-    <header> 
-      First HTML page
-    </header>
-    <footer>
-      Holberton School - 2017
-    </footer>
-  </body>
-</html>
+document.addEventListener('DOMContentLoaded', () => {
+  const header = document.querySelector('header');
+  console.log(header);
+  header.style.color = '#FF0000';
+});
